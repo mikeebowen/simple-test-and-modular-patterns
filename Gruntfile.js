5a65f38a4ee403272437cb6e913843bdb19bee93
Michael Bowen <mikee.bowen@gmail.com>
2015-04-29 11:46:19 -0700

    Add comments to code

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,13 @@
 "use strict";
 // create wrapper function
 module.exports = function (grunt) {
+  //Load tasks
   grunt.loadNpmTasks("grunt-contrib-jshint");
   grunt.loadNpmTasks("grunt-simple-mocha");
   grunt.loadNpmTasks('grunt-contrib-watch');
+  // initialize grunt
   grunt.initConfig({
+    // create jshint task
     jshint: {
       dev: {
         src: ["Gruntfile.js", "greet*.js", "test/**/*.js"]
@@ -13,16 +16,20 @@ module.exports = function (grunt) {
         jshintrc: ".jshintrc"
       },
     },
+    // create simple mocha task
     simplemocha: {
       dev: {
         src: ["test/**/*.js"]
       }
     },
+    // create watch task
     watch: {
       files: ["<%= jshint.dev.src %>"],
       tasks: ["jshint"]
     }
   });
+  // register the created tasks to run as "test"
   grunt.registerTask("test", ["jshint:dev", "simplemocha:dev", "watch"]);
+  // make "test" the default grunt task
   grunt.registerTask("default", ["test"]);
 };
