72aa5959908723c4021f5b8c97a5dd1dea61dbfa
Michael Bowen <mikee.bowen@gmail.com>
2015-04-29 11:23:36 -0700

    Add .jshintrc file and add code for watch to Gruntfile.js and package.json

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,30 +3,26 @@
 module.exports = function (grunt) {
   grunt.loadNpmTasks("grunt-contrib-jshint");
   grunt.loadNpmTasks("grunt-simple-mocha");
+  grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.initConfig({
     jshint: {
       dev: {
         src: ["Gruntfile.js", "greet*.js", "test/**/*.js"]
       },
-
       options: {
-        node: true,
-        globals: {
-          describe: true,
-          it: true,
-          before: true,
-          after: true,
-          beforeEach: true,
-          afterEach: true
-        }
-      }
+        jshintrc: ".jshintrc"
+      },
     },
     simplemocha: {
       dev: {
         src: ["test/**/*.js"]
       }
+    },
+    watch: {
+      files: ["<%= jshint.dev.src %>"],
+      tasks: ["jshint"]
     }
   });
-  grunt.registerTask("test", ["jshint:dev", "simplemocha:dev"]);
+  grunt.registerTask("test", ["jshint:dev", "simplemocha:dev", "watch"]);
   grunt.registerTask("default", ["test"]);
 };
